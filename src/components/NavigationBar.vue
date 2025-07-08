ba6c44e7de10f0181d16c85fa3c0329740711c40
cashums <136028501+cashums@users.noreply.github.com>
2025-07-08 13:44:44 +0900

    Prototype for the Nav Bar operational

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/login">Login</RouterLink>
+    </nav>
+  </header>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
