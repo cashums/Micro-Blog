cb5a26b7832ac2e6e517fae2b049f7ec0e6056b8
cashums <136028501+cashums@users.noreply.github.com>
2025-07-22 21:24:53 +0900

    Updated router links

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import UserProfileView from '../views/UserProfileView.vue'
 import LoginView from '../views/LoginView.vue'
+import ArchiveView from '../views/ArchiveView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: LoginView,
+    },
+    {
+      path: '/archives',
+      name: 'archives',
+      component: ArchiveView,
     },
   ],
 })
